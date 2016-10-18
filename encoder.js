df4f1e87d1b5b12aac253454e58b7892082098ed
Cristian Montellano <cristianmontellano32@gmail.com>
2016-10-18 08:49:30 -0700

    Have started to work on getting Caesar Cipher conversion to work

--- encoder.js
+++ encoder.js
@@ -1,3 +1,36 @@
+//Caeser Cipher Conversion Object
+var CaeserCipherConversion = {
+
+  "A":"Z",
+  "B":"A",
+  "C":"B",
+  "D":"C",
+  "E":"D",
+  "F":"E",
+  "G":"F",
+  "H":"G",
+  "I":"H",
+  "J":"I",
+  "K":"J",
+  "L":"K",
+  "M":"L",
+  "N":"M",
+  "O":"N",
+  "P":"O",
+  "Q":"P",
+  "R":"Q",
+  "S":"R",
+  "T":"S",
+  "U":"T",
+  "V":"U",
+  "W":"V",
+  "X":"W",
+  "Y":"X",
+  "Z":"Y"
+
+}
+
+
 //Checks which button is pressed
 $("input[type='radio']").click(function() {
 
@@ -10,12 +43,28 @@ $("input[type='radio']").click(function() {
 //Follows which button is checked and uses the code needed
 if ($("input:checked").val() == "echo") {
 
+  $("#inputArea").keydown(function() {
 
+    $("#textArea").append("A");
+
+  });
 
 }
 
 else if ($("input:checked").val() == "Caeser Cipher") {
+  $("#inputArea").keydown(function() {
 
+    var Encode = String.fromCharCode(event.which);
+
+    var Endcoder = Encode.split('');
+
+    var Encoded;
+
+    Encoded = CaeserCipherConversion[Encoder[0]];
+
+    $("#textArea").append("Encoded");
+
+  });
 
 
 }
@@ -27,7 +76,7 @@ else if ($("input:checked").val() == "Heiroglyphics") {
 }
 
 
-$("#inputArea").keydown(function(e){
+$("#inputArea").keydown(function(e) {
 
   $("#textArea").append(String.fromCharCode(event.which));
 
